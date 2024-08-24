8ceaffc3aedd89e71b652a4c75bf35815236b90b
khurram waqar <khurrambaigw@gmail.com>
2024-08-24 18:55:10 +0500

    Published Categories Show Only 2

--- routes/Categories.js
+++ routes/Categories.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const CategoryController = require('../controllers/CategoriesController');
 
 router.get('/', CategoryController.getAllCategories);
+router.get('/bkend', CategoryController.getAllCategoriesForBackend);
 router.get('/:categoryId', CategoryController.getSpecificCategory);
 router.post('/', CategoryController.createCategory);
 router.put('/:categoryId', CategoryController.updateCategory);
 router.delete('/:categoryId', CategoryController.deleteCategory);
-router.get('/bkend', CategoryController.getAllCategoriesForBackend)
 module.exports = router;
